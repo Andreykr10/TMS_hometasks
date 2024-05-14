e9186a87f86b23145b117e11f68baf6349a51474
Andrey <andryhakr10@gmail.com>
2024-05-14 18:16:16 +0300

    QA27_2_3

---
+++ src/QA27_2_3/Task1.java
@@ -0,0 +1,11 @@
+package QA27_2_3;
+
+public class Task1 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++){
+           if (i % 2 == 1){
+               System.out.println(i);
+           }
+        }
+    }
+}
