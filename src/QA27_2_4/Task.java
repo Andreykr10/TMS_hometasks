de622e16b7f7b72c781d1f435b03a44971acc058
Andrey <andryhakr10@gmail.com>
2024-05-14 18:23:49 +0300

    QA27_2_4

---
+++ src/QA27_2_4/Task.java
@@ -0,0 +1,10 @@
+package QA27_2_4;
+
+public class Task {
+    public static void main(String[] args) {
+        for  (int i = 5; i > 0; i--){
+            System.out.println(i);
+        }
+
+    }
+}
