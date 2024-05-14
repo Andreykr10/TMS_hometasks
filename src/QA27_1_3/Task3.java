63e9092e4ff3a921fcb35f5efc84bf44752a7ed1
Andrey <andryhakr10@gmail.com>
2024-05-14 16:56:09 +0300

    QA27_1_3, Добавили проверку

--- src/QA27_1_3/Task3.java
+++ src/QA27_1_3/Task3.java
@@ -3,6 +3,7 @@ package QA27_1_3;
 import java.util.Scanner;
 
 public class Task3 {
+    final private static double MAX_X = 20;
     public static void main(String[] args) {
         double a,c,x;
         Scanner in = new Scanner(System.in);
@@ -12,6 +13,8 @@ public class Task3 {
         c=in.nextDouble();
         x=10+c-a;
         System.out.println("x="+x);
+        boolean lessMaxX = x <= MAX_X;
+        System.out.println("x меньше либо равен MAX_X "+lessMaxX);
 
     }
 }
