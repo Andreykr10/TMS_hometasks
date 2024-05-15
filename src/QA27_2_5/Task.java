911a7771e60c85078ca922f0834c91e06eb19624
Andrey <andryhakr10@gmail.com>
2024-05-15 22:44:22 +0300

    QA27_2_5

---
+++ src/QA27_2_5/Task.java
@@ -0,0 +1,16 @@
+package QA27_2_5;
+
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите любое целое положительное число ");
+        int x = in.nextInt();
+        int sum = 0;
+        for  (int i = 1; i <= x; i++){
+            sum = sum + i;
+        }
+            System.out.println(sum);
+    }
+}
